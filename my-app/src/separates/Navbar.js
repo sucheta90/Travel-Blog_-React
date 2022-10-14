36c7181faf2d50f2163ead1f21c72c7f111b9aba
Sucheta Mukherjee <mukherjee.sucheta90@gmail.com>
2022-10-14 11:56:35 -0400

    Changed the globe style

--- my-app/src/separates/Navbar.js
+++ my-app/src/separates/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Globe from '../images/globe.png'
+// import Globe from '../images/globe.png'
 
 
 export default function Navbar(){
     return (
         
         <nav className="navbar align-items-start">
-        <img src={Globe} className="globe"/>
-        <h3 className="page-title"> my travel journal</h3>
+        {/* <img src={Globe} className="globe"/> */}
+        <h3 className="page-title">  <i className="fa-solid fa-globe"></i> My travel journal</h3>
        </nav>
        
     )
